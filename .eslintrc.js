fab826c0c4772ccefebe4b2947da6b694323559b
Serhii Filatov <filser.spirit@gmail.com>
2023-03-07 15:35:03 +0200

    feat: add code

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,18 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: [
-    'plugin:react/recommended',
-    'airbnb',
-  ],
-  overrides: [
-  ],
+  extends: ['plugin:react/recommended', 'airbnb'],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: [
-  ],
+  plugins: [],
   rules: {
+    'linebreak-style': ['error', process.platform === 'win32' ? 'windows' : 'unix'],
   },
 };
