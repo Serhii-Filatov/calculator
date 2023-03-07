01f2ddd5e13111e51aa0373f6c172d88c5862207
Serhii Filatov <filser.spirit@gmail.com>
2023-03-07 21:42:11 +0200

    test: testing error in terminal during call git commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,12 +11,13 @@ module.exports = {
   },
   plugins: [],
   rules: {
-    'linebreak-style': ['error', (process.platform === 'win32' ? 'windows' : 'unix')],
+    // 'linebreak-style': ['error', (process.platform === 'win32' ? 'windows' : 'unix')],
+    'linebreak-style': 0,
     'import/prefer-default-export': 'off',
     'import/named': 'off',
     'class-methods-use-this': 'off',
     'import/no-cycle': 'off',
     'import/extensions': 'off',
-    'import/no-unresolved': 'off'
-  }
+    'import/no-unresolved': 'off',
+  },
 };
