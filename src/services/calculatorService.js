a90919dd275ae7f32d3bdc919cfa74f2fafce5d0
Serhii Filatov <filser.spirit@gmail.com>
2023-04-06 11:08:12 +0300

    refactor: сhanged names and function calls

--- src/services/calculatorService.js
+++ src/services/calculatorService.js
@@ -1,91 +1,88 @@
 import { setOutput } from '../store/calculatorStore.js';
 
-let a = ''; // first number
-let b = ''; // second number
-let sign = ''; // operation
-let finish = false; // isFinished
+let firstNumber = '';
+let secondNumber = '';
+let operation = '';
+let isFinished = false;
 
-const digit = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
+const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '.'];
 const action = ['-', '+', '*', '/'];
 
 // screen
 
 // Логика
 const clearAll = () => {
-  // const out = document.querySelector('.calc-screen span');
-  a = '';
-  b = '';
-  sign = '';
-  finish = false;
+  firstNumber = '';
+  secondNumber = '';
+  operation = '';
+  isFinished = false;
   setOutput(0);
 };
 
 const calculate = (event) => {
-  const out = document.querySelector('.calc-screen span');
-
   // нажата не кнопка
   if (!event.target.classList.contains('btn')) return;
   //  нажата кнопка clearAll ac
   if (event.target.classList.contains('ac')) return;
 
   // Вьюшка
-  out.textContent = '';
+  setOutput('');
 
   // Получаю нажатую кнопку
   const key = event.target.textContent;
   // если нажата кнопка 0-9 или .
-  if (digit.includes(key)) {
-    if (b === '' && sign === '') {
-      a += key;
-      out.textContent = a;
-    } else if (a !== '' && b !== '' && finish) {
-      b = key;
-      finish = false;
-      out.textContent = b;
+  if (digits.includes(key)) {
+    if (secondNumber === '' && operation === '') {
+      firstNumber += key;
+      setOutput(firstNumber);
+    } else if (firstNumber !== '' && secondNumber !== '' && isFinished) {
+      secondNumber = key;
+      isFinished = false;
+      setOutput(secondNumber);
     } else {
-      b += key;
-      out.textContent = b;
+      secondNumber += key;
+      setOutput(secondNumber);
     }
     return;
   }
 
   // если нажата кнопка + - / *
   if (action.includes(key)) {
-    sign = key;
-    out.textContent = sign;
+    operation = key;
+    setOutput(operation);
   }
 
   // нажато =
   if (key === '=') {
-    if (b === '') b = a;
-    switch (sign) {
+    if (secondNumber === '') secondNumber = firstNumber;
+    switch (operation) {
       case '+':
-        a = +a + +b;
+        firstNumber = +firstNumber + +secondNumber;
         break;
       case '-':
-        a -= b;
+        firstNumber -= secondNumber;
         break;
       case '*':
-        a *= b;
+        firstNumber *= secondNumber;
         break;
       case '/':
-        if (b === '0') {
-          out.textContent = 'Ошибка!';
-          a = '';
-          b = '';
-          sign = '';
+        if (secondNumber === '0') {
+          setOutput('Ошибка!');
+          firstNumber = '';
+          secondNumber = '';
+          operation = '';
           return;
         }
-        a /= b;
+        firstNumber /= secondNumber;
         break;
       case '%':
-        a /= 100;
+        firstNumber /= 100;
         break;
       default:
         break;
     }
-    finish = true;
-    out.textContent = a;
+    isFinished = true;
+    setOutput(firstNumber);
   }
 };
 
--- src/store/calculatorStore.js
+++ src/store/calculatorStore.js
@@ -1,4 +1,52 @@
+// import { outComponent } from '../components/outComponent.js';
 export const state = { out: '0' };
 export const setOutput = (out) => {
   state.out = out;
+
+  // UNDONE Избавиться от строк ниже с помощью паттерна наблюдатель
+  const output = document.querySelector('.calc-screen span');
+  output.textContent = state.out;
 };
+// Функция, которая создает наблюдаемый объект
+// const createObservable = () => {
+// наблюдатели
+//   const observers = [];
+
+//   return {
+// Добавить наблюдателя
+//     addObserver(observer) {
+// Добавить наблюдателя в массив наблюдателей
+//       observers.push(observer);
+//     },
+//     notify(data) {
+//       observers.forEach((observer) => observer(data));
+//     },
+//   };
+// };
+
+// Функция, которая создает наблюдателя
+// const createObserver = (observable, observerFn) => {
+//   observable.addObserver(observerFn);
+// };
+
+// Создаем наблюдаемый объект
+// const observable = createObservable();
+
+// Создаем наблюдателей и подписываем их на наблюдаемый объект
+// const observer1 = createObserver(observable, (data) => {
+//   console.log(`Observer1: получены данные - ${data}`);
+// });
+
+// Оповещаем наблюдателей об изменении состояния
+// observable.notify(setOutput(8));
+// observable.notify(setOutput(12));
+
+/*
+Обзервер(собскрайбер Подписчик) и есть твой компонет а стор это обзервебл(Издатель)
+по факту тебе надо 2 функции в сторе
+subscribe() - (Подписаться)
+notify() - (Уведомить)
+дальше ты их экспортируешь
+подписываешься твоим subscribe(outComponent)
+только внутри него. Там должен быть метод на обновление out.textContent
+*/
